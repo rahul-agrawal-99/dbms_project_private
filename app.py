e9904d4afcb56e4713d987f1b6e808b270ae1d6b
rahul99 <agrawal.rahul20022@gmail.com>
2022-01-20 17:54:51 +0530

    updated ui2

--- app.py
+++ app.py
@@ -251,7 +251,7 @@ def add_card():
 
 if __name__ == "__main__":
     # app.run(host='0.0.0.0' , port=5000)
-    app.run(debug=True, port=5000)
+    app.run()
 
 # app.run()            # for Production
 # app.run(debug=True)          # for testing
